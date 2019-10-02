01d2d7e3c9ca96d3c0b65c004005e565e10d3db7
mmnr00 <mmnr00@gmail.com>
2019-10-02 14:44:07 +0800

    t

--- app/views/rails_c/ekidsms.rb
+++ app/views/rails_c/ekidsms.rb
@@ -1,17 +1,5 @@
-id=[7653,
-6330,
-6329,
-6314,
-7587,
-7582,
-6261,
-6256,
-6244,
-6253,
-6231,
-6215,
-6212,
-6213]
+#FREE SCREENING
+id=[7653,6330,6329,6314,7587,7582,6261,6256,6244,6253,6231,6215,6212,6213]
 
 @ekids = Ekid.where(id: id)
 @ekids.each do |ek|
@@ -27,5 +15,29 @@ body: "\n\n MBI Selangor Incorporated
 \n 2. Umur Anak dan No MyKid
 \n 3. No yang boleh dihubungi
 \n TEMPAT ADALAH AMAT TERHAD. Terima kasih atas kerjasama anda."
+)
+end
+
+#MENTORING
+id=[7566, 6182, 6224, 6156, 6166, 6174, 6173, 6168, 6163, 6160]  
+
+@ekids = Ekid.where(id: id)
+@ekids.each do |ek|
+@client = Twilio::REST::Client.new(ENV["TWILIO_ACCOUNT_SID"], ENV["TWILIO_AUTH_KEY"])
+@client.messages.create(
+to: "+60#{ek.mph}", 
+from: ENV["TWILIO_PHONE_NO"], 
+body: "\n\n MBI Selangor Incorporated 
+\n Terima kasih kerana mendaftar untuk Program Saringan Percuma anjuran MBI Selangor (INC).
+\n Tahniah! #{ek.name} telah disenaraipendek untuk program mentoring dan coaching. Untuk makluman, program seterusnya akan diadakan seperti berikut:
+\n 22 September 2019 (Ahad)
+\n 8.30am- 5.30ptg
+\n SACC MALL, Shah Alam.
+\n Sila whatsapp maklumat dibawah kepada urusetia Program, Pn Zaiha di 013-668 9376
+\n 1. Nama Anak
+\n 2. Umur Anak dan No MyKid
+\n 3. No yang boleh dihubungi
+\n 4. Jenis Kelewatan Perkembangan Anak
+\n TEMPAT ADALAH AMAT TERHAD. HUBUNGI KAMI SEGERA.Terima kasih atas kerjasama anda."
 )
 end
